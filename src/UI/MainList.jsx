b27385ad9bf86c762a996a1e1f8cb6ef1fb7874b
tyzctyzc2 <40290383+tyzctyzc2@users.noreply.github.com>
2021-05-08 09:07:55 +0800

    video add
    add video

--- src/UI/MainList.jsx
+++ src/UI/MainList.jsx
@@ -1,5 +1,6 @@
 import textIcon from '../text.png';
 import binaryIcon from '../binary.png';
+import { Modal, Button } from 'antd';
 import '../App.css';
 import React, { Component } from 'react';
 import '../config';
@@ -13,7 +14,9 @@ export default class MainList extends React.Component{
   constructor(){
       super();
       this.state = {
-          banners: ['']
+          banners: [''],
+          isModalVisible:false,
+          curImg: null
       }
   }
 
@@ -76,9 +79,14 @@ export default class MainList extends React.Component{
   }
 
   renderIconImage(file) {
+    if (file == undefined) {
+      return textIcon;
+    }
     switch(file.fileType) {
       case 'png':
       case 'jpg':
+      case 'mov':
+        //return "http://localhost:8080\\ydjm\\" + file.filePath + "\\" + file.fileName;
         return ".\\ydjm\\" + file.filePath + "\\" + file.fileName;
       case 'txt':
         return textIcon;
@@ -87,8 +95,23 @@ export default class MainList extends React.Component{
     }
   }
 
+  handleZoom = (e) => {
+    console.log(e)
+    let { clientHeight, clientWidth, style } = e.target
+    let ratio = clientHeight / clientWidth
+    console.log("ratio===" + ratio)
+    console.log("clientHeight===" + clientHeight)
+    console.log("clientWidth===" + clientWidth)
+    if (e.nativeEvent.deltaY <= 0 && clientWidth < 1000) {
+        style.transform = "scale(2.5)"
+    } else if (e.nativeEvent.deltaY > 0) {
+        style.transform = "scale(1)"
+    }
+  }
+
   render(){
       return(
+        <div>
           <div id="list_root">
             <h1>{currentSelection}</h1>
               {
@@ -114,10 +137,19 @@ export default class MainList extends React.Component{
                         {
                           element.files && element.files.map((file, ii) =>{
                             return(
-                              <div key = {ii}>
+                              <div key = {ii} name = {ii} className="fullLine">
                                 {
-                                  file.fileName ? 
-                                  <img className = "iconImg" src={this.renderIconImage(file)}/>: ''
+                                  file.fileType == 'jpg' ?
+                                  <img  className = "iconImg" 
+                                        src={this.renderIconImage(file)}
+                                        onClick={()=>{this.setState({isModalVisible:true, curImg:file})}}
+                                  />: ''
+                                }
+                                {
+                                  file.fileType == 'mov' ?
+                                  <video id="video"  width="640" height="480" muted controls autoplay="autoplay" preload="auto" >
+                                    <source src={this.renderIconImage(file)} />
+                                  </video>: ''
                                 }
                               </div>
                             )
@@ -130,6 +162,21 @@ export default class MainList extends React.Component{
               }
             
           </div>
+          <Modal  fullScreen title="TAG" visible={this.state.isModalVisible}
+                onCancel={()=>{this.setState({
+                    isModalVisible:false
+                })}}
+                footer={[
+                   
+                  ]}
+                >
+                  <div onWheel={this.handleZoom}>
+                    <img  className = "iconImg" 
+                          src={this.renderIconImage(this.state.curImg)}
+                    />
+                  </div>
+                </Modal>
+        </div>
       )
   }
 }
